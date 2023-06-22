35143935b78b285b2b23ee1399fbd53379289e58
mnkhod <mnkhod.dev@gmail.com>
2023-06-22 23:59:27 +0800

    Added Proof of Concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tokens, setTokens] = useState([]);
+
+  useEffect(() => {
+    getAddressDetail();
+  }, []);
+
+  async function getAddressDetail() {
+    try {
+      let tokenAddress = "bc1pus3ue90xcrq2sh7tlfaghw89rlgvr9986xh86qemct85zq2v839q7mmve2"
+      let tokenTicker = "BTOC"
+
+      let response = await axios({
+        method: "get",
+        url:
+          `https://unisat.io/brc20-api-v2/address/${tokenAddress}/brc20/${tokenTicker}/history?start=0&limit=20`,
+      });
+
+      if (response.data.msg == "ok") {
+        let tokenData = response.data.data;
+        let resultTokens = tokenData.detail;
+        setTokens(resultTokens);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-full flex justify-center items-center">
+      <ul className="flex gap-5 flex-col">
+        {tokens.length > 0 && tokens.map((token) => (
+          <li className="flex flex-col gap-3 p-12 border">
+            <h1>{token.ticker}</h1>
+            <p>type - [{token.type}]</p>
+            <p>from - {token.from}</p>
+            <p>to - {token.to}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
