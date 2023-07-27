c86fc87e1c45a0a8ec84c8c6b16a6f0bf43bfc04
ccireedui <ireedui355@gmail.com>
2023-07-27 21:19:27 +0800

    changed data source api

--- src/App.jsx
+++ src/App.jsx
@@ -24,41 +24,88 @@ function App() {
     try {
       let response = await axios({
         method: "get",
-        url: `https://unisat.io/brc20-api-v2/address/${raffle.userAddress}/brc20/${raffle.tokenTicker}/history?start=0&limit=512&type=receive`,
+        headers: {
+          "Ok-Access-Key": import.meta.env.VITE_OKLINK_API_KEY,
+        },
+        url: `https://www.oklink.com/api/v5/explorer/btc/transaction-list?toAddress=${raffle.userAddress}&limit=0&token=${raffle.tokenTicker}`,
       });
 
-      if (response.data.msg == "ok") {
-        let tokenData = response.data.data;
-        let resultTransfers = tokenData.detail;
-        let result = [];
-        let endTime = moment(raffle.endTime, raffle.timeFormat);
-        // let startTime = moment(raffle.startTime, raffle.timeFormat);
-        for (let i = 0; i < resultTransfers.length; i++) {
-          if (
-            endTime.unix() < resultTransfers[i].blocktime
-            // || startTime.unix() > resultTransfers[i].blocktime
-          )
-            continue;
-          let user = resultTransfers[i].from;
-          let amount = parseInt(resultTransfers[i].amount);
-          let userExists = result.find((obj) => obj.from === user);
-          if (userExists) {
-            userExists.amount += amount;
-            userExists.ticket = Math.floor(
-              userExists.amount / raffle.ticketPrice
-            );
-          } else {
-            result.push({
-              // Use push to add new objects to the array
-              from: user,
-              amount: amount,
-              ticket: Math.floor(amount / raffle.ticketPrice),
-            });
+      let tokenDatas = response.data.data;
+
+      let result = [];
+      let endTime = moment(raffle.endTime, raffle.timeFormat);
+      // let startTime = moment(raffle.startTime, raffle.timeFormat);
+      if (tokenDatas.length == 0) return;
+      for (let i = 0; i < tokenDatas.length; i++) {
+        for (let j = 0; j < parseInt(tokenDatas[i].totalPage); j++) {
+          for (let k = 0; k < tokenDatas[i].inscriptionsList.length; k++) {
+            if (
+              endTime.unix() <
+              tokenDatas[i].inscriptionsList[k].time / 1000
+            ) {
+              continue;
+            }
+            let user = tokenDatas[i].inscriptionsList[k].fromAddress;
+            let amount = parseInt(tokenDatas[i].inscriptionsList[k].amount);
+            let userExists = result.find((obj) => obj.fromAddress === user);
+            if (userExists) {
+              userExists.amount += amount;
+              userExists.ticket = Math.floor(
+                userExists.amount / raffle.ticketPrice
+              );
+            } else {
+              result.push({
+                // Use push to add new objects to the array
+                from: user,
+                amount: amount,
+                ticket: Math.floor(amount / raffle.ticketPrice),
+              });
+            }
           }
         }
-        result.sort((a, b) => b.ticket - a.ticket);
-        setTokens(result);
       }
+      result.sort((a, b) => b.ticket - a.ticket);
+      setTokens(result);
+
+      // unisat added cors
+
+      // let response = await axios({
+      //   method: "get",
+      //   url: `https://unisat.io/brc20-api-v2/address/${raffle.userAddress}/brc20/${raffle.tokenTicker}/history?start=0&limit=512&type=receive`,
+      // });
+
+      // if (response.data.msg == "ok") {
+      //   let tokenData = response.data.data;
+      //   let resultTransfers = tokenData.detail;
+      //   let result = [];
+      //   let endTime = moment(raffle.endTime, raffle.timeFormat);
+      //   // let startTime = moment(raffle.startTime, raffle.timeFormat);
+      //   for (let i = 0; i < resultTransfers.length; i++) {
+      //     if (
+      //       endTime.unix() < resultTransfers[i].blocktime
+      //       // || startTime.unix() > resultTransfers[i].blocktime
+      //     )
+      //       continue;
+      //     let user = resultTransfers[i].from;
+      //     let amount = parseInt(resultTransfers[i].amount);
+      //     let userExists = result.find((obj) => obj.from === user);
+      //     if (userExists) {
+      //       userExists.amount += amount;
+      //       userExists.ticket = Math.floor(
+      //         userExists.amount / raffle.ticketPrice
+      //       );
+      //     } else {
+      //       result.push({
+      //         // Use push to add new objects to the array
+      //         from: user,
+      //         amount: amount,
+      //         ticket: Math.floor(amount / raffle.ticketPrice),
+      //       });
+      //     }
+      //   }
+      //   result.sort((a, b) => b.ticket - a.ticket);
+      //   setTokens(result);
+      // }
     } catch (e) {
       console.log(e);
     }
