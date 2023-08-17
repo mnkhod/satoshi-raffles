a82c2758ac665f2b365a5a584e319f210f39ee4b
ccireedui <ireedui355@gmail.com>
2023-08-17 22:28:41 +0800

    changed casing to camel case, added alt for close img

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -30,6 +30,7 @@ export default function ConnectWallet({ onClose }) {
               <Image
                 src={CloseImg}
                 className="cursor-pointer ml-6"
+                alt="Close icon"
                 onClick={onClose}
               />
             </div>
@@ -78,9 +79,9 @@ export default function ConnectWallet({ onClose }) {
                     <path
                       d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
                       stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     ></path>
                   </svg>
                 </div>
@@ -114,9 +115,9 @@ export default function ConnectWallet({ onClose }) {
                     <path
                       d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
                       stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     ></path>
                   </svg>
                 </div>
@@ -150,9 +151,9 @@ export default function ConnectWallet({ onClose }) {
                     <path
                       d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
                       stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     ></path>
                   </svg>
                 </div>
